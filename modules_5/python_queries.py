d3e658f0447b53811159d0613b3aafdb442d40b8
eddiemac922 <88352564+eddiemac922@users.noreply.github.com>
2021-08-30 11:15:49 -0500

    Update python_queries.py

--- modules_5/python_queries.py
+++ modules_5/python_queries.py
@@ -1,1 +1,24 @@
+###IMPORTS
+from pymongo import MongoClient
+import certifi
+
+###MAIN
+
+if __name__ == "__main__":
+    url = "mongodb+srv://admin:admin@cluster0.ayo4d.mongodb.net/pytech?retryWrites=true&w=majority"
+    client = MongoClient(url, tlsCAFile=certifi.where())
+    db = client.pytech
+    students = db.students
+
+###TOTAL QUERY
+    print("-- DISPLAYING DOCUMENTS FROM STUDENT COLLECTION WITH find() QUERY --\n")
+    for s in students.find():
+        print(f"\tStudent ID: {s['student_id']}\n\tFirst Name: {s['first_name']}\n\tLast Name: {s['last_name']}\n")
+
+###SINGLE QUERY
+    print("-- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --\n")
+    s = students.find_one({"student_id": 1007})
+    print(f"\tStudent ID: {s['student_id']}\n\tFirst Name: {s['first_name']}\n\tLast Name: {s['last_name']}\n")
+
+    input("\nEnd of program, press any key to exit...\n")
 
