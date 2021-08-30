330a9c803e7344cc703a211e7d85312f4f86074e
eddiemac922 <88352564+eddiemac922@users.noreply.github.com>
2021-08-30 11:12:07 -0500

    Update python_inserts.py

--- modules_5/python_inserts.py
+++ modules_5/python_inserts.py
@@ -1,1 +1,27 @@
+###IMPORTS
+
+from pymongo import MongoClient
+import certifi
+
+###MAIN
+
+if __name__ == "__main__":
+    url = "mongodb+srv://admin:admin@cluster0.ayo4d.mongodb.net/pytech?retryWrites=true&w=majority"
+    client = MongoClient(url, tlsCAFile=certifi.where())
+    db = client.pytech
+    studentColl = db.students
+
+###STUDENTS
+    
+students = [{"student_id": 1007,"first_name" : "Mike", "last_name" : "Jones"},
+            {"student_id": 1008,"first_name" : "Eddie", "last_name" : "Kain"},
+            {"student_id": 1009,"first_name" : "Timmy", "last_name" : "Turner"}]
+###INSERTS
+
+print("-- INSERT STATEMENTS --")
+for student in students:
+    Object_id = studentColl.insert_one(student)
+    print(f"Inserted student {student['first_name']} {student['last_name']} into the students collections with document id {Object_id.inserted_id}.")
+
+input("\nEnd of program, press any key to exit... ")
 
